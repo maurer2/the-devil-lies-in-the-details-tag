1aaebde143cbbed123ae6018f4ae9f44c6735dc3
Martin <mr.mmaurer@gmail.com>
2025-06-04 23:39:56 +0200

    Added AccordionToggleButtons component
    # Conflicts:
    #	src/components/Accordion/styles.css.ts

--- src/components/Accordion/styles.css.ts
+++ src/components/Accordion/styles.css.ts
@@ -9,34 +9,3 @@ export const detailsWrapper = style({
   containerName: detailsWrapperContainer,
   containerType: 'inline-size',
 });
-
-export const toggleButtonGroup = style({
-  display: 'flex',
-  gap: roundWithFallback('calc(var(--spacing-default) * 1)'),
-  marginBottom: roundWithFallback('calc(var(--spacing-default) * 1)'),
-  flexWrap: 'wrap',
-});
-
-export const toggleButton = style({
-  paddingInline: roundWithFallback('calc(var(--spacing-default) * 1)'),
-  paddingBlock: roundWithFallback('calc(var(--spacing-default) * 0.75)'),
-  border: '1px solid var(--color-primary)',
-  backgroundColor: 'var(--color-secondary)',
-  opacity: 1,
-  cursor: 'pointer',
-  transition: 'opacity 0.25s',
-  flexBasis: '100%',
-
-  selectors: {
-    '&[aria-disabled=true]': {
-      opacity: 0.25,
-      cursor: 'not-allowed',
-    },
-  },
-
-  '@container': {
-    [`${detailsWrapperContainer} (width > 400px)`]: {
-      flexBasis: 'auto',
-    },
-  },
-});
