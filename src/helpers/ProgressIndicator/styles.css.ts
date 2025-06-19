96d188e039bbdc8cff4230a37669380128f9e067
Martin <mr.mmaurer@gmail.com>
2025-06-20 00:13:31 +0200

    Added css scroll progress animation

--- src/helpers/ProgressIndicator/styles.css.ts
+++ src/helpers/ProgressIndicator/styles.css.ts
@@ -24,27 +24,29 @@ const progressKeyframes = keyframes({
 export const progressIndicator = style({
   aspectRatio: '1 / 1',
   position: 'fixed',
+  zIndex: 10,
   top: roundWithFallback('calc(var(--spacing-default) * 1)'),
   right: roundWithFallback('calc(var(--spacing-default) * 1)'),
   display: 'none',
   isolation: 'isolate',
-  opacity: 0.25,
   borderRadius: '50%',
-  backgroundColor: 'var(--color-primary)',
+  backgroundColor: 'var(--color-tertiary)',
   width: roundWithFallback('calc(var(--spacing-default) * 2)'),
-  color: 'var(--color-primary)',
+  color: 'var(--color-tertiary)',
 
   '@supports': {
-    '(mask-composite: intersect)': {
+    '(mask-composite: intersect) and (animation-timeline: scroll())': {
       display: 'block',
-      animation: `${progressKeyframes} 10s infinite`,
+      animation: `${progressKeyframes} linear`,
+      animationTimeline: 'scroll(root)',
       maskComposite: 'intersect',
       maskSize: '100%',
 
       // 1. mask center of circle
       // 2. mask border that is not filled yet
+      // todo: remove magic numbers
       maskImage: `
-    radial-gradient(circle at center, transparent 0, transparent 60%, currentColor 60%, currentColor 100%),
+    radial-gradient(circle at center, transparent 0, transparent 50%, currentColor 50%, currentColor 100%),
     conic-gradient(currentColor 0deg, currentColor ${progressAsDeg}, transparent ${progressAsDeg})`,
     },
   },
